f63313b2cf3da9af1eb02703b0f6ac2f78b86d02
= <bhavesh0594.cse19@chitkara.edu.in>
2021-06-03 23:02:30 +0530

    Fix the mobile view

--- script.js
+++ script.js
@@ -6,14 +6,22 @@ rocket.addEventListener("mouseenter", () => {
   var vh = document.documentElement.clientHeight;
   var chgLeft = Math.floor(Math.random() * (vw / 2));
   var chgTop = Math.floor(Math.random() * (vh / 2));
-  if (vw <= 400 && vh <= 600) {
-      vw = vw - chgLeft - 200;
-      vh = vh - chgTop - 100;
-  } else {
+
+  //setting up the random position for mobile view
+  if (vw <= 450 || vh <= 800) {
+    vw = vw - chgLeft - 200;
+    vh = vh - chgTop - 300;
+  } else {    //setting up the random position for pc view
     vw = vw - chgLeft - 500;
     vh = vh - chgTop - 400;
   }
 
+  if (vw <= -20 || vh <=0) { //avoiding the rcoket to go beyond the screen dimensions
+    vw = 20;
+    vh = 10;
+  }
+
+  //setting the final vh & vw to the rocket
   rocket.style.top = `${vh}px`;
   rocket.style.left = `${vw}px`;
 });
