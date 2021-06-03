1a9bf0edd60b64e0caa13c9eee674b2e5c11f56a
= <bhavesh0594.cse19@chitkara.edu.in>
2021-06-03 20:18:01 +0530

    Fix mobile view

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-var rocket =  document.querySelector("i");
+var rocket = document.querySelector("i");
 
-rocket.addEventListener("mouseenter",()=>{
-    rocket.classList.toggle("fa-rotate");
-    var vw = document.documentElement.clientWidth;
-    var vh = document.documentElement.clientHeight;
-    var chgLeft = Math.floor(Math.random() * (vw/2));
-    var chgTop = Math.floor(Math.random() * (vh/2));
-    vw = vw- chgLeft - 500;
+rocket.addEventListener("mouseenter", () => {
+  rocket.classList.toggle("fa-rotate");
+  var vw = document.documentElement.clientWidth;
+  var vh = document.documentElement.clientHeight;
+  var chgLeft = Math.floor(Math.random() * (vw / 2));
+  var chgTop = Math.floor(Math.random() * (vh / 2));
+  if (vw <= 400 && vh <= 600) {
+      vw = vw - chgLeft - 200;
+      vh = vh - chgTop - 100;
+  } else {
+    vw = vw - chgLeft - 500;
     vh = vh - chgTop - 400;
-    
-    rocket.style.top = `${vh}px`;
-    rocket.style.left = `${vw}px`;
-})
+  }
 
+  rocket.style.top = `${vh}px`;
+  rocket.style.left = `${vw}px`;
+});
